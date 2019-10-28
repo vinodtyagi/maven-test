2c2d8dac0e28950a9d95258cbcc935aac2eef410
vinod <vinod.tyagi222@gmail.com>
2019-10-28 18:46:33 +0530

    commit for maven test project

---
+++ src/main/java/skillnet/App.java
@@ -0,0 +1,13 @@
+package skillnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
